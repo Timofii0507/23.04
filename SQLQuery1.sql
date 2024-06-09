2953e9cfc2a1079c5f85636ecc3c01ac640aa531
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-06-09 11:50:20 +0300

    select_script_hm

---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿USE SportStore;
+GO
+
+EXEC GetAllProductsInfo;
+GO
+
+EXEC GetTop3OldestCustomers;
+GO
+
+EXEC GetTopSellingEmployee;
+GO
+
+DECLARE @Manufacturer NVARCHAR(100) = 'SomeManufacturer';
+DECLARE @Available BIT;
+
+EXEC CheckManufacturerAvailability @Manufacturer, @Available OUTPUT;
+
+IF @Available = 1
+    PRINT 'Yes, products from manufacturer ' + @Manufacturer + ' are available.';
+ELSE
+    PRINT 'No, products from manufacturer ' + @Manufacturer + ' are not available.';
+
+EXEC GetTopSellingManufacturer;
+GO
+
+DECLARE @Date DATE = '2023-01-01';
+DECLARE @DeletedCount INT;
+
+EXEC DeleteCustomersRegisteredAfterDate @Date, @DeletedCount OUTPUT;
+
+PRINT 'Deleted ' + CAST(@DeletedCount AS NVARCHAR(10)) + ' customers registered after ' + CAST(@Date AS NVARCHAR(10)) + '.';
