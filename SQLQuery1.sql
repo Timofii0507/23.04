9af43e097e55e3a2bb409830b1dc76b828094a84
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-06-09 11:52:52 +0300

    update_script-hm

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+﻿USE SportStore;
+GO
+
+UPDATE Products
+SET Quantity = 12,
+    Cost = 55.00
+WHERE ProductID = 1;
+GO
+
+UPDATE Employees
+SET Position = 'Senior Sales Associate',
+    Salary = 40000.00
+WHERE EmployeeID = 1;
+GO
+
+UPDATE Customers
+SET Email = 'new_email@example.com',
+    Phone = '1231231234'
+WHERE CustomerID = 1;
+GO
+
+UPDATE Sales
+SET SalePrice = 80.00
+WHERE SaleID = 1;
+GO
